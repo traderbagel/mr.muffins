1c670575bdf58e959dca202f87be03308074ea70
chuchuhao <chuchuhao831@gmail.com>
2017-06-25 15:40:43 +0800

    turn to line api

--- app.py
+++ app.py
@@ -3,62 +3,63 @@ import json
 import requests
 import os 
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 app = Flask(__name__)
 
 # Some python config method :
 # https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
 
 
-DATABASE_CONFIG = {
-	'channel_id':'1521764494', # UserID
-    'channel_secret':'74c0cf68a0fd5fd6f1f68ca12c83c2f6', # UserSecrertKey
-	'channel_MID':'Z6kGvct04yThOXOacWqg72dqExNO39KmKGmibbAQ2wN2yCbtnR6vx0Qq9MbCdUusC3CAsQSDcqRBEFDmO3qIImfdit5jsFV4CmcqmVViR0kSyR5ZugOB56Rhm1JKUOOkSkgPVncMfwJxBlB9up8qkwdB04t89/1O/w1cDnyilFU=' # MID
+API_CONFIG = {
+	'channel_id':'1521764494',
+    'channel_secret':'74c0cf68a0fd5fd6f1f68ca12c83c2f6',
+	'channel_access_token':'Z6kGvct04yThOXOacWqg72dqExNO39KmKGmibbAQ2wN2yCbtnR6vx0Qq9MbCdUusC3CAsQSDcqRBEFDmO3qIImfdit5jsFV4CmcqmVViR0kSyR5ZugOB56Rhm1JKUOOkSkgPVncMfwJxBlB9up8qkwdB04t89/1O/w1cDnyilFU='
 }
+
+
+line_bot_api = LineBotApi(API_CONFIG['channel_access_token'])
+handler = WebhookHandler(API_CONFIG['channel_secret'])
+
 
 @app.route('/')
 def index():
     return "<p>Hello World!</p>"
 
-
-# @app.route('/callback', methods=['POST'])
-@app.route('/', methods=['POST'])
+# https://github.com/line/line-bot-sdk-python
+@app.route('/callback', methods=['POST'])
 def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded['result'][0]['content']['from']
-    text = decoded['result'][0]['content']['text']
-    #print(json_line)
-    print("user:",user)
-    print("content:",text)
-    sendText(user,text)
-    return ''
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-def sendText(user, text):
-    LINE_API = 'https://trialbot-api.line.me/v1/events'
-    CHANNEL_ID = DATABASE_CONFIG['channel_id']
-    CHANNEL_SECRET = DATABASE_CONFIG['channel_secret']
-    MID = DATABASE_CONFIG['channel_MID']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
 
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'X-Line-ChannelID': CHANNEL_ID,
-        'X-Line-ChannelSecret': CHANNEL_SECRET,
-        'X-Line-Trusted-User-With-ACL': MID
-    }
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
 
-    data = json.dumps({
-        "to": [user],
-        "toChannel":1383378250,
-        "eventType":"138311608800106203",
-        "content":{
-            "contentType":1,
-            "toType":1,
-            "text":text
-        }
-    })
+    return 'OK'
 
-    r = requests.post(LINE_API, headers=headers, data=data)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)) 
+
 
 if __name__ == '__main__':
     # http://kennmyers.github.io/tutorial/2016/03/11/getting-flask-on-heroku.html
