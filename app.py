0ffadf2d39bc675bf3ca1f41480ca744b1f17862
chuchuhao <chuchuhao831@gmail.com>
2017-06-27 23:38:36 +0800

    why can't send image

--- app.py
+++ app.py
@@ -75,8 +75,7 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 ImageSendMessage(
-                    original_content_url=str(request.url_root)+'/origin_weather',
-                    preview_image_url   =str(request.url_root)+'/origin_weather'
+                    original_content_url=str(request.url_root)+'/origin_weather'
                 )
             )
         else:
